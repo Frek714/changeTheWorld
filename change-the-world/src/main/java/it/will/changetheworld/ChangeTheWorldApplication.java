856e19c5b1a1917cf3a492e6b9fbebdb97f1c00c
FedericoDursi <federico.dursi1998@gmail.com>
2020-01-15 15:41:07 +0100

    added spring boot application empty

---
+++ change-the-world/src/main/java/it/will/changetheworld/ChangeTheWorldApplication.java
@@ -0,0 +1,13 @@
+package it.will.changetheworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeTheWorldApplication.class, args);
+	}
+
+}
---
+++ change-the-world/src/test/java/it/will/changetheworld/ChangeTheWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package it.will.changetheworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeTheWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
